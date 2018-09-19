020cdcc038542537ca2cf4a7b778bba76dae8afe
Tay Blank <tayblank@L15-USStudent-LBlankenship18-MBP-2.local>
2018-09-19 13:42:55 -0400

    modified input values

--- Lab4.java
+++ Lab4.java
@@ -10,7 +10,8 @@ import java.util.Scanner;
 public class Lab4 {
     
     public static void main(String[] args)
-    {   String[] ans=null;
+    {   
+        /*
         if(args.length==0)
         {   Scanner in=new Scanner(System.in);
             System.out.println("No arguments found. Please re-enter the items"
@@ -20,6 +21,33 @@ public class Lab4 {
         else{ans=sort(args);}
         for(String s:args)
         {System.out.println(s);}
+        */
+        System.out.println("Input: 5 3 7 9 0");
+        String[] a={"5","3","7","9","0"};a=sort(a);
+        System.out.print("Output:");
+        for(String s:a){System.out.print(" "+s);}
+        
+        System.out.println("\nInput: 5 6 7 3 2 1");
+        String[] b={"5","6","7","3","2","1"};b=sort(b);
+        System.out.print("Output:");
+        for(String s:b){System.out.print(" "+s);}
+        
+        System.out.println("\nInput: 4 3 2 8 6");
+        String[] c={"4","3","2","8","6"};c=sort(c);
+        System.out.print("Output:");
+        for(String s:c){System.out.print(" "+s);}
+        
+        System.out.println("\nInput: 4 0 9 3");
+        String[] d={"4","0","9","3"};d=sort(d);
+        System.out.print("Output:");
+        for(String s:d){System.out.print(" "+s);}
+        
+        System.out.println("\nInput: 7 8 5 1 3 6");
+        String[] e={"7","8","5","1","3","6"};e=sort(e);
+        System.out.print("Output:");
+        for(String s:e){System.out.print(" "+s);}
+        
+        System.out.println();
     }
     
     /**
